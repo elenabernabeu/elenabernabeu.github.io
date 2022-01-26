a1a4487f8f86dcbe75cc6366f8fbc28354dbf7cf
elenabernabeu <bgelenabg@hotmail.com>
2022-01-26 14:39:37 +0000

    Improved pandoc conversion for pdf

--- pdf_cv/pandoc_cv.sh
+++ pdf_cv/pandoc_cv.sh
@@ -2,6 +2,7 @@
 
 pandoc ../cv.markdown \
        -V block-headings \
+       --pdf-engine=xelatex \
        --shift-heading-level-by=-2 \
        --template templates/resume.latex \
        -f markdown+yaml_metadata_block \
